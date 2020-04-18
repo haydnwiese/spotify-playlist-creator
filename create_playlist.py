828bce792f1f6c6527f69590a495e02c03eea96a
haydnwiese <45501692+haydnwiese@users.noreply.github.com>
2020-04-18 00:19:39 -0400

    Implemented functions for creating playlist and searching for song

--- create_playlist.py
+++ create_playlist.py
@@ -1,8 +1,13 @@
+import json
+import requests
+from secrets import spotify_user_id, spotify_token
+
 
 class CreatePlaylist:
 
     def __init__(self):
-        pass
+        self.user_id = spotify_user_id
+        self.spotify_token = spotify_token
 
     def get_youtube_client(self):
         pass
@@ -11,10 +16,51 @@ class CreatePlaylist:
         pass
 
     def create_playlist(self):
-        pass
+        request_body = json.dumps({
+            "name": "YouTube Liked Videos",
+            "description": "All Liked YouTube Videos",
+            "public": True
+        })
 
-    def get_spotify_uri(self):
-        pass
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(self.user_id)
+        response = requests.post(
+            query,
+            data=request_body,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(spotify_token)
+            }
+        )
+        response_json = response.json()
+
+        # Playlist id
+        return response_json["id"]
+
+    def get_spotify_uri(self, song_name, artist):
+        """Search For the Song"""
+        query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
+            song_name,
+            artist
+        )
+        response = requests.get(
+            query,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(spotify_token)
+            }
+        )
+        response_json = response.json()
+        songs = response_json["tracks"]["items"]
+
+        # only use the first song
+        uri = songs[0]["uri"]
+
+        return uri
 
     def add_song_to_playlist(self):
         pass
+
+
+if __name__ == '__main__':
+    cp = CreatePlaylist()
+    cp.create_playlist()
